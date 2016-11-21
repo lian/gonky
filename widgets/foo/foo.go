ebf2a62a84fa6fe1f5bb2c1c6a6b10400f04aa9b
Julian Langschaedel <meta.rb@gmail.com>
2016-11-21 17:36:12 +0100

    add mono6x13

--- widgets/foo/foo.go
+++ widgets/foo/foo.go
@@ -4,6 +4,7 @@ import (
 	"image"
 	"image/color"
 
+	"github.com/lian/gonky/font/mono6x13"
 	"github.com/lian/gonky/font/terminus"
 	"github.com/lian/gonky/texture"
 	"github.com/llgcode/draw2d/draw2dimg"
@@ -28,8 +29,12 @@ func (s *Foo) Render() {
 		gc.Fill()
 	*/
 
-	terminus.DrawString(data, 20, 150, "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\"", color.Black)
-	terminus.DrawString(data, 20, 80, "Go's standard library provides strong support for \ninterpreting UTF-8 text. If a for range loop isn't sufficient for your purposes,\nchances are the facility you need is provided by a package in the library.", color.Black)
+	terminus.DrawString(data, 20, 10, "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\"", color.Black)
+	mono6x13.DrawString(data, 20, 30, "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\"", color.Black)
+
+	terminus.DrawString(data, 20, 50, "Go's standard library provides strong support for \ninterpreting UTF-8 text. If a for range loop isn't sufficient for your purposes,\nchances are the facility you need is provided by a package in the library.", color.Black)
+
+	mono6x13.DrawString(data, 20, 100, "Go's standard library provides strong support for \ninterpreting UTF-8 text. If a for range loop isn't sufficient for your purposes,\nchances are the facility you need is provided by a package in the library.", color.Black)
 
 	s.Texture.Write(&data.Pix)
 }
